5b11195a297b90dd503689da3698fffa0defd0d8
wbienabe <wbienabe@laposte.net>
2015-01-06 15:18:58 +0100

    Encodage page d'accueil

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 <h2>Bienvenue</h2>
 <p>
-    Bienvenue sur <em>Social Sondage</em>, ici, vous pourrez créer et participer
-    à des sondages!
+    Bienvenue sur <em>Social Sondage</em>, ici, vous pourrez crÃ©er et participer
+    Ã  des sondages!
 </p>
 
-<h2>Comment ça marche?</h2>
+<h2>Comment Ã§a marche?</h2>
 <p>
-    Premièrement, <a href="register.php">inscrivez-vous</a>, vous devez avoir un 
-    compte pour participer. <a href="create.php">Créez ensuite</a> votre sondage en ligne.
+    PremiÃ¨rement, <a href="register.php">inscrivez-vous</a>, vous devez avoir un 
+    compte pour participer. <a href="create.php">CrÃ©ez ensuite</a> votre sondage en ligne.
     Vous obtiendrez un lien, en allant dans la rubrique <a href="my-polls.php">mes sondages</a>
     que vous pourrez partager pour que les autres puissent participer.
 </p>
