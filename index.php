21c0499a816c28d6dd4af0dad9a7c6d0de7c7c3b
wbienabe <wbienabe@laposte.net>
2015-01-16 23:00:09 +0100

    manquait quelques fichiers

--- index.php
+++ index.php
@@ -1,17 +1,215 @@
-<?php include('header.php'); ?>
+<?php
 
-<h2>Bienvenue</h2>
-<p>
-    Bienvenue sur <em>Social Sondage</em>, ici, vous pourrez créer et participer
-    à des sondages!
-</p>
+//app.request.resquestURI
+//use Symfony\Component\HttpFoundation\Request;
 
-<h2>Comment ça marche?</h2>
-<p>
-    Premièrement, <a href="register.php">inscrivez-vous</a>, vous devez avoir un 
-    compte pour participer. <a href="create.php">Créez ensuite</a> votre sondage en ligne.
-    Vous obtiendrez un lien, en allant dans la rubrique <a href="my-polls.php">mes sondages</a>
-    que vous pourrez partager pour que les autres puissent participer.
-</p>
+$loader = include('vendor/autoload.php');
+$loader->add('', 'src');
 
-<?php include('footer.php'); ?>
+$app = new Silex\Application();
+$app['debug'] = true;
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__ . '/views'));
+
+$app['model'] = new Sondages\Model(
+        '127.0.0.1', // Hôte
+        'td5_gregwar', // Base de données
+        'root', // Utilisateur
+        ''     // Mot de passe
+);
+/*
+  $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+  'db.options' => array(
+  'driver' => 'pdo_mysql',
+  'dbhost' => '127.0.0.1',
+  'dbname' => 'td5_gregwar',
+  'user' => 'root',
+  'password' => '',
+  ),
+  ));
+
+  $app['userManager'] = $app->share(function() use ($app) {
+  return new Sondages\UserManager($app['db'], $app);
+  }); */
+
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('home.html.twig', array(
+                'user'=>$app['model']->checkConnection($app['session']->get('user'), $app)            
+    ));
+})->bind('home');
+
+/*
+  $app->register(new Silex\Provider\SecurityServiceProvider(), array(
+  'security.firewalls' => array(
+  'foo' => array('pattern' => '^/foo'), // Example of an url available as anonymous user
+  'default' => array(
+  'pattern' => '^.*$',
+  'anonymous' => true, // Needed as the login path is under the secured area
+  'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+  'logout' => array('logout_path' => '/logout'), // url to call for logging out
+  'users' => $app->share(function() use ($app) {
+  return new Sondages\UserProvider($app['db']);
+  }),
+  ),
+  ),
+  'security.access_rules' => array(
+  array('^/create', 'ROLE_USER'),
+  array('^/foo', ''), // This url is available as anonymous user
+  )
+  )); */
+
+$app->match('/login', function() use ($app) {
+    if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
+        if (!empty(filter_input(INPUT_POST, 'login') && !empty(filter_input(INPUT_POST, 'password')))) {
+            $app['model']->login(filter_input(INPUT_POST, 'login'), filter_input(INPUT_POST, 'password'), $app);
+        }
+    }
+    return $app['twig']->render('login.html.twig', array(
+                'user'=>$app['model']->checkConnection($app['session']->get('user'))
+    ));
+})->bind('login');
+
+$app->match('/logout', function() use ($app) {
+    $app['session']->clear();
+    return $app->redirect('/');
+})->bind('logout');
+
+$app->match('/register', function() use ($app) {
+    if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'POST') {
+        if ((null !== filter_input(INPUT_POST, 'login')) && (null !== filter_input(INPUT_POST, 'password'))) {
+            $boolRegister = $app['model']->register(filter_input(INPUT_POST, 'login'), filter_input(INPUT_POST, 'password'));
+            return $app['twig']->render('register.html.twig', array(
+                        'register'=>$boolRegister,
+                        'missingField'=>false)
+            );
+        }
+    }
+    return $app['twig']->render('register.html.twig', array(
+                'missingField'=>true));
+})->bind('register');
+
+// Ajouter un sondage
+/* $app->match('/create', function() use ($app) {
+  if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'POST') {
+  if (!empty(filter_input(INPUT_POST, 'question')) &&
+  !empty(filter_input(INPUT_POST, 'answer1')) &&
+  !empty(filter_input(INPUT_POST, 'answer2'))) {
+
+  $question = filter_input(INPUT_POST, 'question');
+  $answer1 = filter_input(INPUT_POST, 'answer1');
+  $answer2 = filter_input(INPUT_POST, 'answer2');
+  $answer3 = filter_input(INPUT_POST, 'answer3');
+  return $app['twig']->render('create.html.twig', array(
+  'formOK'=>$app['model']->addPoll($question, $answer1, $answer2, $answer3, $app['session']->get('user'))));
+  }
+  else {
+  return $app['twig']->render('create.html.twig', array(
+  'formOK'=>false));
+  }
+  }
+  return $app['twig']->render('create.html.twig');
+  })->bind('create'); */
+
+$app->match('/create', function() use ($app) {
+    /* echo "<script>alert('[" . filter_input(INPUT_POST, 'answer2') . "]')</script>";
+      if ((filter_input(INPUT_POST, 'answer3') == '')) {
+      //echo "<script>alert('[" . filter_input(INPUT_POST, 'answer3') . "]')</script>";
+      echo "<script>alert('ya pas');</script>";
+      } */
+
+    if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'POST') {
+        if (!empty(filter_input(INPUT_POST, 'question')) &&
+                !empty(filter_input(INPUT_POST, 'answer1')) &&
+                !empty(filter_input(INPUT_POST, 'answer2'))) {
+            /* while (filter_input(INPUT_POST, 'answer' . $nbAnswers) !== '') {
+              echo "<script>alert('[" . filter_input(INPUT_POST, 'answer' . $nbAnswers) . "]')</script>";
+              echo "<script>alert('" . $nbAnswers . "')</script>";
+              $nbAnswers ++;
+              } */
+            $reponses = array();
+            for ($nbReponses = 1; $nbReponses <= 10; $nbReponses++) {
+                $reponse = filter_input(INPUT_POST, 'answer' . $nbReponses);
+                if ($reponse != '') {
+                    array_push($reponses, $reponse);
+                }
+            }
+
+            $question = filter_input(INPUT_POST, 'question');
+
+            return $app['twig']->render('create.html.twig', array(
+                        'formOK'=>$app['model']->addPoll($question, implode("|", $reponses), $app['session']->get('user'))));
+        }
+        else {
+            return $app['twig']->render('create.html.twig', array(
+                        'formOK'=>false));
+        }
+    }
+    return $app['twig']->render('create.html.twig');
+})->bind('create');
+
+$app->match('/polls', function() use ($app) {
+    return $app['twig']->render('polls.html.twig', array(
+                'polls'=>$app['model']->getPolls(),
+                'user'=>$app['model']->checkConnection($app['session']->get('user'))
+    ));
+})->bind('polls');
+
+$app->match('/pollsId/{id}', function($id) use ($app) {
+    if (!empty(filter_input(INPUT_POST, 'answer'))) {
+        if (!$app['model']->didIAnswer($app['session']->get('user'), $id)) {
+            $app['model']->addAnswer($app['session']->get('user'), $id, filter_input(INPUT_POST, 'answer'));
+        }
+        else {
+            echo "<script>alert('Tout doux, vous avez déjà voté');</script>";
+        }
+    }
+    $answers = $app['model']->getPossiblesAnswers($id);
+    $total = $app['model']->getAnswersCount($id);
+    return $app['twig']->render('pollsId.html.twig', array(
+                'userAnswered'=>$app['model']->didIAnswer($app['session']->get('user'), $id),
+                'polls'=>$app['model']->getPollsFromId($id),
+                'answers'=>$answers,
+                'answersNb'=>$app['model']->getAnswersPct($answers, $id) ,
+                'total'=>$app['model']->getTotalAnswers($total)
+    ));
+})->bind('pollsId');
+
+$app->match('/myPolls', function() use ($app) {
+
+
+    return $app['twig']->render('myPolls.html.twig', array(
+                'user'=>$app['model']->checkConnection($app['session']->get('user')),
+                'sondages'=>$app['model']->getMyPolls($app['session']->get('user'))
+    ));
+})->bind('myPolls');
+
+/*
+  $app->post('/user/{action}', function ($action) use ($app) {
+  switch ($action) {
+  case 'createUser':
+  $result = $app['userManager']->insertNewUser(filter_input(INPUT_POST, 'login'), filter_input(INPUT_POST, 'password'), filter_input(INPUT_POST, 'role'));
+  $returnArray = array(
+  'insertingResult' => $result
+  );
+  return json_encode($returnArray);
+  case 'deleteUser':
+  $result = $app['userManager']->deleteUser(filter_input(INPUT_POST, 'id'));
+  $returnArray = array(
+  'deletingResult' => $result
+  );
+  return json_encode($returnArray);
+  default:
+  return false;
+  }
+  })->bind('userAction'); */
+
+// Fait remonter les erreurs
+$app->error(function($error) {
+    throw $error;
+});
+
+//$app->boot();
+$app->run();
--- src/Sondages/Model.php
+++ src/Sondages/Model.php
@@ -10,7 +10,8 @@ class Model {
         try {
             $this->pdo = new \PDO('mysql:dbname=' . $database . ';host=' . $host, $user, $password);
             $this->pdo->exec('SET CHARSET UTF8');
-        } catch (\PDOException $exception) {
+        }
+        catch (\PDOException $exception) {
             die('Impossible de se connecter au serveur MySQL : ' . $exception->getMessage());
         }
     }
@@ -36,7 +37,7 @@ class Model {
     }
 
     public function getPollsFromId($id) {
-         /* $sql = 'SELECT * FROM polls WHERE id=?';
+        /* $sql = 'SELECT * FROM polls WHERE id=?';
           var_dump($id);
           $query = $this->pdo->prepare($sql);
           $query->execute(array($id));
@@ -44,6 +45,14 @@ class Model {
         // moche mais je truc de dessus marche pas.. pas compris
         $sql = 'SELECT * FROM polls WHERE id = ' . $id;
         return $this->pdo->query($sql);
+    }
+
+    public function getIdFromPoll($poll) {
+        $sql = 'SELECT id FROM polls WHERE question = ?';
+        $query = $this->pdo->prepare($sql);
+        $query->execute(array($poll));
+        //var_dump($query->fetch());
+        return $query->fetch();
     }
 
     public function getIdFromUser($user) {
@@ -83,7 +92,8 @@ class Model {
         $query->execute(array(htmlspecialchars($login), md5(htmlspecialchars($password))));
         if ($query->rowCount()) {
             return false;
-        } else {
+        }
+        else {
             $sql = 'INSERT INTO users (login, password) VALUES (?, ?)';
             $query = $this->pdo->prepare($sql);
             $query->execute(array(htmlspecialchars($login), md5(htmlspecialchars($password))));
@@ -94,30 +104,82 @@ class Model {
     protected function fetchOne(\PDOStatement $query) {
         if ($query->rowCount() != 1) {
             return false;
-        } else {
+        }
+        else {
             return $query->fetch();
         }
     }
 
-    function getTotal($pollId) {
-        $answers = array();
-        foreach (array(1, 2, 3) as $answer) {
-            $sql = 'SELECT COUNT(*) as nb FROM answers WHERE poll_id = ? AND answer = ?';
-            $query = $this->pdo->prepare($sql);
-            $query->execute(array($pollId, $answer));
+    /* function getTotal($pollId) {
+      $answers = array();
+      foreach (array(1, 2, 3) as $answer) {
+      $sql = 'SELECT COUNT(*) as nb FROM answers WHERE poll_id = ? AND answer = ?';
+      $query = $this->pdo->prepare($sql);
+      $query->execute(array($pollId, $answer));
 
-            $result = $query->fetch();
-            $answers[$answer] = $result['nb'];
-        }
-        $total = array_sum($answers);
-        
-        return array('total' => $total, 'answers' => $answers);
+      $result = $query->fetch();
+      $answers[$answer] = $result['nb'];
+      }
+      $total = array_sum($answers);
+      return array('total'=>$total, 'answers'=>$answers);
+      } */
+
+    function getTotalAnswers($total) {
+       // var_dump(count($total));
+        return count($total);
     }
-    
-    function addPoll($question, $answer1, $answer2, $answer3) {
-        $sql = 'INSERT INTO polls (question, answer1, answer2, answer3) VALUES (?, ?, ?, ?)';
+
+    function getCountOfAnswer($id, $cpt) {
+        $sql = 'SELECT COUNT(answer) as nb FROM answers WHERE poll_id = ? AND answer = ?';
         $query = $this->pdo->prepare($sql);
-        $query->execute(array($question, $answer1, $answer2, $answer3));
+        $query->execute(array($id, $cpt));
+        $result = $query->fetch();
+        //print_r($result);
+        if ($result) {
+            return $result['nb'];
+        }
+        return 0;
+    }
+
+    function getPossiblesAnswers($pollId) {
+        $sql = 'SELECT answers FROM polls WHERE id = ?';
+        $query = $this->pdo->prepare($sql);
+        $query->execute(array($pollId));
+        $result = $query->fetch();
+        if ($result) {
+            return explode("|", $result['answers']);
+        }
+        return 0;
+    }
+
+    function getAnswersPct($answers, $id) {
+        $nbTab = array();
+        //var_dump($answers);
+        $cpt = 1;
+        foreach ($answers as $answer) {
+            array_push($nbTab, $this->getCountOfAnswer($id, $cpt));
+            $cpt++;
+        }
+        // print_r($nbTab);
+        return $nbTab;
+    }
+
+    function getAnswersCount($pollId) {
+        $countTab = array();
+        $sql = 'SELECT answer FROM answers WHERE poll_id = ?';
+        $query = $this->pdo->prepare($sql);
+        $query->execute(array($pollId));
+        while ($res = $query->fetch()) {
+            array_push($countTab, $res["answer"]);
+        }
+        //var_dump($countTab);
+        return $countTab;
+    }
+
+    function addPoll($question, $answers, $auteur) {
+        $sql = 'INSERT INTO polls (question, answers, auteur_sondage) VALUES (?, ?, ?)';
+        $query = $this->pdo->prepare($sql);
+        $query->execute(array($question, $answers, $auteur));
         return true;
     }
 
